19a0b6d31e2ee3e3678654472ca472ca4d1a4989
ChriswellPetii <nypeti13@gmail.com>
2018-08-02 13:02:10 +0200

    added details about me

---
+++ week-01/day-4/IntroduceYourself/src/IntroduceYourself.java
@@ -0,0 +1,17 @@
+public class IntroduceYourself {
+    public static void main(String[] args) {
+        // Write a program that prints a few details to the terminal window about you
+        // It should print each detail to a new line.
+        //  - Your name
+        //  - Your age
+        //  - Your height in meters (as a decimal fraction)
+        //
+        //  Example output:
+        //  John Doe
+        //  31
+        //  1.87
+        System.out.println("Nyilas Péter");
+        System.out.println("21 y");
+        System.out.println("1.72m ");
+    }
+}
