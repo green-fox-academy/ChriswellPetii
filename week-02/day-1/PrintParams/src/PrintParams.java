11219593f52362a2532d18f46de805b2f0f318cf
ChriswellPetii <nypeti13@gmail.com>
2018-08-06 15:55:27 +0200

    feat(PrintParams): finish task

---
+++ week-02/day-1/PrintParams/src/PrintParams.java
@@ -0,0 +1,28 @@
+public class PrintParams {
+    public static void main(String[] args) {
+        //  Create the usual class wrapper
+        //  and main method on your own.
+
+        // - Create a function called `printParams`
+        //   which prints the input String parameters
+        // - It can have any number of parameters
+
+        // Examples
+        // printParams("first")
+        // printParams("first", "second")
+        // printParams("first", "second", "third", "fourth")
+
+        String[] a = {"first", "second", "third", "fourth"};
+
+        printParams(a);
+
+    }
+
+    public static void printParams(String[] a) {
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] +" ");
+        }
+
+    }
+}
