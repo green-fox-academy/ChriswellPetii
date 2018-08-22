6ee48f785481e61a82dc2515aa935dcb8d3cd57c
ChriswellPetii <nypeti13@gmail.com>
2018-08-22 14:50:15 +0200

    feat(Zoo): finish task

---
+++ week-04/day-3/Zoo/src/animals/Bird.java
@@ -0,0 +1,12 @@
+package animals;
+
+public class Bird extends Animal {
+    public Bird(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String breed() {
+        return "laying eggs.";
+    }
+}
