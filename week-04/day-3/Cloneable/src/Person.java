268ac4cf879374af0e63f7dd05834cd6e032a000
ChriswellPetii <nypeti13@gmail.com>
2018-08-22 13:18:41 +0200

    feat(Cloneable): finish task

---
+++ week-04/day-3/Cloneable/src/Person.java
@@ -0,0 +1,37 @@
+public class Person {
+    private String name;
+    private int age;
+    private String gender;
+
+    Person(String name, int age, String gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    Person() {
+        this.name = "Jane Doe";
+        this.age = 30;
+        this.gender = "female";
+    }
+
+    public void introduce() {
+        System.out.println("Hi, I'm " + this.name + ", a " + this.age + " year old " + this.gender + ".");
+    }
+
+    public void getGoal() {
+        System.out.println("My goal is: Live for the moment!");
+    }
+
+    String getName() {
+        return this.name;
+    }
+
+    int getAge() {
+        return this.age;
+    }
+
+    String getGender() {
+        return this.gender;
+    }
+}
---
+++ week-04/day-3/Cloneable/src/Student.java
@@ -0,0 +1,38 @@
+public class Student extends Person implements Cloneable {
+    private String previousOrganization;
+    private int skippedDays;
+
+    Student() {
+        super();
+        this.previousOrganization = "The School of Life";
+    }
+
+    Student(String name, int age, String gender, String previousOrganization) {
+        super(name, age, gender);
+        this.previousOrganization = previousOrganization;
+    }
+
+    @Override
+    public void getGoal() {
+        System.out.println("My goal is: Be a junior software developer.");
+    }
+
+    public void introduce() {
+        System.out.println("Hi, I'm " + getName() + ", a " + getAge() + " year old " + getGender() + " from " + this.previousOrganization + " who skipped " + this.skippedDays + " days from the course already.");
+
+    }
+
+    public void skipDays(int numberOfDays) {
+        this.skippedDays += numberOfDays;
+    }
+
+    @Override
+    public Object clone() {
+        try {
+            return super.clone();
+        } catch (CloneNotSupportedException e) {
+            System.out.println("Clone not supported");
+        }
+        return null;
+    }
+}
