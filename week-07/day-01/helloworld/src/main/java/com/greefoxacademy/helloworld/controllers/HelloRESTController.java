e12fc8ca157ff7448f5c42c1805ecfb5efead7b6
ChriswellPetii <nypeti13@gmail.com>
2018-09-17 14:02:41 +0200

    feat(HelloWorldREST): @RequestParam

--- week-07/day-01/helloworld/src/main/java/com/greefoxacademy/helloworld/controllers/HelloRESTController.java
+++ week-07/day-01/helloworld/src/main/java/com/greefoxacademy/helloworld/controllers/HelloRESTController.java
@@ -2,13 +2,14 @@ package com.greefoxacademy.helloworld.controllers;
 
 import com.greefoxacademy.helloworld.Greeting;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloRESTController {
     @RequestMapping("/greeting")
-    public Greeting greeting() {
-        Greeting greeting = new Greeting(1, "Hello World!");
+    public Greeting greeting(@RequestParam("name") String name) {
+        Greeting greeting = new Greeting(1, "Hello " + name + "!");
         return greeting;
     }
 }
