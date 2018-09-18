440bd41809d075508054bada691bcddf8e76522f
ChriswellPetii <nypeti13@gmail.com>
2018-09-18 17:34:36 +0200

    feat(RaiseTheBalance): inputs created, button created

--- week-07/day-02/day-02/src/main/java/com/greenfoxacademy/day02/controllers/BankAccountController.java
+++ week-07/day-02/day-02/src/main/java/com/greenfoxacademy/day02/controllers/BankAccountController.java
@@ -3,6 +3,8 @@ package com.greenfoxacademy.day02.controllers;
 import com.greenfoxacademy.day02.models.BankAccount;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
@@ -14,7 +16,7 @@ public class BankAccountController {
 
     @RequestMapping("/show")
     public String account(Model model) {
-        model.addAttribute("account", new BankAccount("Nala", 2000, "lion", false,true));
+        model.addAttribute("account", new BankAccount("Nala", 2000, "lion", false, true));
         return "index";
     }
 
@@ -24,13 +26,19 @@ public class BankAccountController {
         return "endpoint";
     }
 
-    @RequestMapping("/lolk")
-    public String valami(Model model) {
+    @PostMapping("/lolk")
+    public String lionKing(Model model) {
         bankAccountList.add(new BankAccount("Simba", 2500, "lion", true, true));
-        bankAccountList.add(new BankAccount("Timon", 300, "meerkat", false,true));
-        bankAccountList.add(new BankAccount("Pumba", 50, "warthog", false,true));
-        bankAccountList.add(new BankAccount("Scar", 2250, "lion", false,false));
-        bankAccountList.add(new BankAccount("Mufasa", 50, "lion", false,true));
+        bankAccountList.add(new BankAccount("Timon", 300, "meerkat", false, true));
+        bankAccountList.add(new BankAccount("Pumba", 50, "warthog", false, true));
+        bankAccountList.add(new BankAccount("Scar", 2250, "lion", false, false));
+        bankAccountList.add(new BankAccount("Mufasa", 50, "lion", false, true));
+        model.addAttribute("balist", bankAccountList);
+        return "listoflionkings";
+    }
+
+    @GetMapping("/lolk")
+    public String lionking(Model model) {
         model.addAttribute("balist", bankAccountList);
         return "listoflionkings";
     }
