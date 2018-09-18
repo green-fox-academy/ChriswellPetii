320f739a4f34603d5d6dc283354a533e6aaae545
ChriswellPetii <nypeti13@gmail.com>
2018-09-18 13:30:21 +0200

    feat(FillingMultileAccounts): finish task

--- week-07/day-02/day-02/src/main/java/com/greenfoxacademy/day02/controllers/BankAccountController.java
+++ week-07/day-02/day-02/src/main/java/com/greenfoxacademy/day02/controllers/BankAccountController.java
@@ -5,8 +5,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class BankAccountController {
+    List<BankAccount> bankAccountList = new ArrayList<>();
+
     @RequestMapping("/show")
     public String account(Model model) {
         model.addAttribute("account", new BankAccount("Simba", 2000, "lion"));
@@ -17,5 +22,16 @@ public class BankAccountController {
     public String endPointing(Model model) {
         model.addAttribute("string", "This is an <em>HTML</em> text. <b>Enjoy yourself!</b>");
         return "endpoint";
+    }
+
+    @RequestMapping("/lolk")
+    public String valami(Model model) {
+        bankAccountList.add(new BankAccount("Nala", 2500, "lion"));
+        bankAccountList.add(new BankAccount("Timon", 300, "meerkat"));
+        bankAccountList.add(new BankAccount("Pumba", 50, "warthog"));
+        bankAccountList.add(new BankAccount("Scar", 2250, "lion"));
+        bankAccountList.add(new BankAccount("Mufasa", 50, "lion"));
+        model.addAttribute("balist", bankAccountList);
+        return "listoflionkings";
     }
 }
