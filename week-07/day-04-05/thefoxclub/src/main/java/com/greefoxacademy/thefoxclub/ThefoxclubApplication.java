f913087ade95382cc6f3ef3ef1e5a356c2ece189
ChriswellPetii <nypeti13@gmail.com>
2018-09-20 10:16:56 +0200

    feat(TheFoxClub): Introduction finished

---
+++ week-07/day-04-05/thefoxclub/src/main/java/com/greefoxacademy/thefoxclub/ThefoxclubApplication.java
@@ -0,0 +1,12 @@
+package com.greefoxacademy.thefoxclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThefoxclubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThefoxclubApplication.class, args);
+    }
+}
