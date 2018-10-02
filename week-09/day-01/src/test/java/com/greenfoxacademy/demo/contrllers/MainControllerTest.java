41253299ccc9c540a1c527196bbdee8df999ccd0
ChriswellPetii <nypeti13@gmail.com>
2018-10-02 14:50:53 +0200

    added AppendA test for the MainController

--- week-09/day-01/src/test/java/com/greenfoxacademy/demo/contrllers/MainControllerTest.java
+++ week-09/day-01/src/test/java/com/greenfoxacademy/demo/contrllers/MainControllerTest.java
@@ -1,37 +1,31 @@
 package com.greenfoxacademy.demo.contrllers;
 
 import com.greenfoxacademy.demo.controllers.MainController;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
-import org.springframework.test.web.servlet.result.JsonPathResultMatchers;
-import org.springframework.validation.support.BindingAwareModelMap;
 
-import static org.junit.Assert.assertEquals;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static sun.nio.cs.Surrogate.is;
+import java.nio.charset.Charset;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(MainController.class)
 public class MainControllerTest {
+
+    private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+            MediaType.APPLICATION_JSON.getSubtype(),
+            Charset.forName("utf8"));
+
 
     @Autowired
     MockMvc mockMvc;
 
-    @Before
-    public void before() {
-
-    }
 
     @Test
     public void testMainLoad() throws Exception {
@@ -39,26 +33,45 @@ public class MainControllerTest {
                 .andExpect(status().isOk());
     }
 
-    @Test
-    public void index() {
-        MainController controller = new MainController();
-        String result = controller.getMainPage();
-        assertEquals("index", result);
-    }
 
     @Test
     public void testDoubling() throws Exception {
         mockMvc.perform(get("/doubling?input=2"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType("application/json;charset=UTF-8"))
+                .andExpect(content().contentType(contentType))
                 .andExpect(jsonPath("result").value(4));
     }
 
     @Test
-    public void testGreeter() throws Exception{
+    public void testGreeter() throws Exception {
         mockMvc.perform(get("/greeter?name=Peter&title=SuperSaiyanGod"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType("application/json;charset=UTF-8"))
+                .andExpect(content().contentType(contentType))
                 .andExpect(jsonPath("welcome_message").value("Oh, hi there Peter, my dear SuperSaiyanGod!"));
+    }
+
+    @Test
+    public void testAppandA() throws Exception {
+        mockMvc.perform(get("/appenda/{appended}", "kuty"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("appended").value("kutya"));
+    }
+
+    @Test
+    public void testDoUntil()throws Exception{
+        mockMvc.perform(post("/dountil/{action}","sum")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"until\" : \"5\"}"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(contentType))
+                .andExpect(jsonPath("result").value("15"));
+
+        mockMvc.perform(post("/dountil/{action}","factor")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"until\" : \"5\"}"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(contentType))
+                .andExpect(jsonPath("result").value("120"));
+
     }
 }
